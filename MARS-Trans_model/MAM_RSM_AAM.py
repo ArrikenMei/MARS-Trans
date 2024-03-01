92ce5513fd19b1e305fe66d15549a11906c652d9
ArrikenMei <87056567+ArrikenMei@users.noreply.github.com>
2024-03-01 17:36:22 +0800

    Update MAM_RSM_AAM.py

--- MARS-Trans_model/MAM_RSM_AAM.py
+++ MARS-Trans_model/MAM_RSM_AAM.py
@@ -332,7 +332,7 @@ class Block_new(nn.Module):
         w_cls, w_without_cls = torch.split(w, [1, dim2-1], dim=2)
 
         # Another strategy is to supplement the image itself
-        # x_cls, x_without_cls = torch.split(x, [1, x.shape[1]-1], dim=2)
+        # x_cls, x_without_cls = torch.split(x, [1, x.shape[1]-1], dim=1)
 
         w_split_batch = torch.split(w_without_cls, 1, dim=0)
         w_cls_batch = torch.split(w_cls, 1, dim=0)
